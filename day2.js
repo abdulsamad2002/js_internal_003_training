076790207fa3ce50e8642a65b289751f870b0ac7
Abdul Samad <168121492+abdulsamad2002@users.noreply.github.com>
2025-12-09 05:06:45 +0000

    day2 of training

---
+++ day2.js
@@ -0,0 +1,43 @@
+// let a = 23
+// const b ="abc"
+// var c = 78
+// d = 98
+
+// console.log(a)
+// console.log(b)
+// console.log(c)
+// console.log(d)
+
+// a = 32
+// // b = "xyz" 
+// c = "87"
+// d = 89
+
+// console.log(a)
+// console.log(b)
+// console.log(c)
+// console.log(d)
+
+
+// let a = null
+// console.log(typeof(a))
+
+// let b
+// console.log(typeof(b))
+
+
+let a = "abc"
+let b =""
+let c = null
+let d
+
+console.log(a)
+console.log(b)
+console.log(c)
+console.log(d)
+
+
+console.log(typeof(undefined))
+console.log(typeof(b))
+console.log(typeof(c))
+console.log(typeof(d))
