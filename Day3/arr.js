8d6fa87707c8fd9b53257a462b99a49b4eb3fda8
Abdul Samad <abdulsamadsjc2002@gmail.com>
2025-12-10 10:49:22 +0530

    Day3

--- Day3/arr.js
+++ Day3/arr.js
@@ -5,37 +5,64 @@
 // console.log(arr)
 
 
-arr = ["Apple", "Banana", "Watermelon", "Pineapple", "Kiwi"]
+// arr = ["Apple", "Banana", "Watermelon", "Pineapple", "Kiwi"]
 
-arr.push("Papaya")
+// arr.push("Papaya")
 
-arr.pop()
+// arr.pop()
 
-arr.unshift("Musk Melon")
-
-
-
-for(a=0; a<arr.length; a++){
-    console.log(arr[a])
-}
-console.log("-------------------------------")
-
-arr.shift("")
-
-for(a=0; a<arr.length; a++){
-    console.log(arr[a])
-}
+// arr.unshift("Musk Melon")
 
 
-console.log(arr.includes("Mango"))
 
-console.log(arr.indexOf("Pineapple"))
+// for(a=0; a<arr.length; a++){
+//     console.log(arr[a])
+// }
+// console.log("-------------------------------")
+
+// arr.shift("")
+
+// for(a=0; a<arr.length; a++){
+//     console.log(arr[a])
+// }
 
 
-let number = [2, 7,3,9,4, 6,213412,353, 345]
+// console.log(arr.includes("Mango"))
 
-let newNum = number.filter((n)=> n%2 != 0)
+// console.log(arr.indexOf("Pineapple"))
 
-console.log(newNum)
+
+// let number = [2, 7,3,9,4, 6,213412,353, 345]
+
+// let newNum = number.filter((n)=> n%2 != 0)
+
+// console.log(newNum)
 
 // array.reduce((accumulator, currentValue)=>{...}, initialValue)
+
+// Term Meaning
+
+// accumulator (acc) => stores the result as the loop continues
+// currentValue (val) => Current element of array being processed
+// initialValue (initialValue) => starting value for accumulation
+
+
+
+const arr = [2,4,6,8,50,30]
+
+// const newArr = arr.map((value)=>{
+//     return value*2
+// })
+
+// const newArr = arr.filter((value)=>{
+//     return value>20
+// })
+
+let sum = 0
+const newArr = arr.reduce((sum, value)=>{
+    return sum+value
+})
+
+console.log(newArr)
+
+
